70624594e7dc9e40250eaa6b27b42f97cfab71a0
Mohamed AboDaif <Mohamed.AboDaif@ppc.com.eg>
2025-08-17 11:50:08 +0300

    e2e-test: update frontend url

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,7 +3,7 @@ const webpackConfig = require("./cypress/webpack.config");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:80",
+    baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:3000",
     supportFile: false,
   },
   component: {
